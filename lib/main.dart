7795d13637ca634e58d1851d7a2d943350558b43
Ohgyuchan <gyu3637@gmail.com>
2021-10-14 20:23:33 +0900

    Add kakao native app key

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:kakao_flutter_sdk/all.dart';
 import 'kakao_login_page.dart';
 
 void main() {
-  KakaoContext.clientId = '카카오 네이티브 앱 키';
+  KakaoContext.clientId = 'ce44c872defc27e4a37af4f4e1a88f93';
   runApp(MyApp());
 }
 
